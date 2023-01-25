fec71be2ac446033ab54d14b0c5ebddbb7db671b
Katharina Washüttl <coding.katharina@gmail.com>
2023-01-25 16:02:21 +0100

    styling added

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let autoCorrectField
 
 inputField.setAttribute("id", "input");
 inputField.setAttribute("type", "text");
+inputField.classList.add("search")
 
 root.appendChild(inputField);
 
@@ -25,7 +26,7 @@ function checkForCities(str){
         .then (data => {
             console.log (data);
             for (let el of data){
-                if (el.name.startsWith(str)){
+                if (el.name.toLowerCase().startsWith(str.toLowerCase())){
                     addAutoCorrect(el)
                 }
             }
@@ -50,6 +51,19 @@ function removeAutoCorrectOptions(){
     }
 }
 
+function getCurrentDate (){
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let today = `${day}.${month}.${year}`;
+    console.log("hi");
+    return today;
+
+}
+
 function displayWeatherCard(city){
     fetch(`http://api.weatherapi.com/v1/current.json?key=84bfaaf9b255449480d172558230301&q=${city.name}&aqi=no`)
         .then (res => res.json())
@@ -59,12 +73,31 @@ function displayWeatherCard(city){
             card.setAttribute("id", "card");
             root.appendChild(card);
         
-            let p = document.createElement("p");
-            card.appendChild(p);
-            p.innerText=
-            `Temperature: ${data.current.feelslike_c}°C\n
-            ${data.current.condition.text}\n
-            Windspeed: ${data.current.gust_kph} kph`;
+            // let weatherData = document.createElement("p");
+            // card.appendChild(weatherData);
+            // weatherData.innerText=
+            // `Temperature: ${data.current.feelslike_c}°C\n
+            // ${data.current.condition.text}\n
+            // Windspeed: ${data.current.gust_kph} kph`;
+
+            let elementToday = document.createElement("p");
+            elementToday.innerText = `Today, ${getCurrentDate()} in ${city.name} it is`;
+            elementToday.classList.add("date");
+            card.appendChild(elementToday);
+
+            let elementTemperature = document.createElement("p");
+            elementTemperature.innerText = `${data.current.feelslike_c}°C`;
+            elementTemperature.classList.add("temperature");
+            card.appendChild(elementTemperature);
+
+            let elementWeatherCondition = document.createElement("p");
+            elementWeatherCondition.innerText = `${data.current.condition.text}`;
+            card.appendChild(elementWeatherCondition);
+
+            let elementWindspeed = document.createElement("p");
+            elementWindspeed.innerText = `Windspeed: ${data.current.gust_kph} kph`;
+            card.appendChild(elementWindspeed);
+
         });
 }
 
